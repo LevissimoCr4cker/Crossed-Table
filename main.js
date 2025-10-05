1a401363bcccb46b7db73c385216c0151dbf5ed2
LevissimoCr4cker <leticiaandrea2025@outlook.com>
2025-10-05 14:18:04 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-xx
+async function loadSchools() {
+  const response = await fetch('./data/schools.json');
+  const schools = await response.json();
+  return schools;
+}
+
+loadSchools().then(schools => {
+  console.log('Loaded', schools.length, 'schools');
+});
