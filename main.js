5f60d71fcacc3706c1540f4923e2fa08bd18a83f
LevissimoCr4cker <leticiaandrea2025@outlook.com>
2025-10-08 14:39:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,3 +7,8 @@ async function loadSchools() {
 loadSchools().then(schools => {
   console.log('Loaded', schools.length, 'schools');
 });
+
+  // Open classification stats page
+    document.getElementById('statsBtn').addEventListener('click', () => {
+      window.location.href = 'stats.html';
+    });
